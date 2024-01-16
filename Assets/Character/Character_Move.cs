1380d55848c7656eaf368ecaec73cb0ca94a3c2e
wind12wind <dbscjfwn@gmail.com>
2024-01-16 14:04:55 +0900

    [Add]Player_Jump_input

--- Assets/Character/Character_Move.cs
+++ Assets/Character/Character_Move.cs
@@ -4,16 +4,22 @@ using UnityEngine.InputSystem;
 public class CharacterMove : MonoBehaviour
 {
     public float moveSpeed = 5.0f; // 이동 속도
+    private Rigidbody rb;          // Rigidbody 컴포넌트
     private Vector2 moveInput;     // 이동 입력
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>(); // Rigidbody 컴포넌트를 가져옵니다
+    }
 
     private void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         Vector3 moveVector = new Vector3(moveInput.x, 0, moveInput.y) * moveSpeed;
-        transform.Translate(moveVector * Time.deltaTime);
+        rb.MovePosition(rb.position + moveVector * Time.fixedDeltaTime);
     }
 }
