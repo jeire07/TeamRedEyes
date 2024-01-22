4dccdb85073816d1390e05057eba59a085a28397
taeseongHan <taeseong0505@naver.com>
2024-01-22 20:29:19 +0900

    [feat] add inventory, ItemSlot
    Inventory, ItemSlot method

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
+using UnityEditorInternal.Profiling.Memory.Experimental;
 using UnityEngine;
 
 public class Inventory : Singleton<Inventory>
@@ -11,7 +13,7 @@ public class Inventory : Singleton<Inventory>
 
     private void Awake()
     {
-        itemSlot = new ItemSlot[10];
+        //itemSlot = new ItemSlot[10]; 슬롯 지정이 필요가 없나???
     }
 
     private void Start()
@@ -42,9 +44,58 @@ public class Inventory : Singleton<Inventory>
 
     public void AddItem(ItemData item)
     {
-       if(itemSlot == null)
-       {
-            
-       }
+        if(item.canStack)
+        {
+            ItemSlot slotTostackTo = GetItemStack(item);
+            if(slotTostackTo != null)
+            {
+                slotTostackTo.quantity++;
+                UpdateUI();
+                return;
+            }
+        }
+
+        ItemSlot emptySlot = GetEmptySlot();
+
+        if(emptySlot != null)
+        {
+            emptySlot.item = item;
+            emptySlot.quantity = 1;
+            UpdateUI();
+            return;
+        }
+    }
+
+    void UpdateUI()
+    {
+        for (int i = 0; i < itemSlot.Length; i++)
+        {
+            if (itemSlot[i].item != null)
+                itemSlot[i].Set(itemSlot[i]);
+            else
+                itemSlot[i].Clear();
+        }
+    }
+
+    ItemSlot GetItemStack(ItemData item)
+    {
+        for (int i = 0; i < itemSlot.Length; i++)
+        {
+            if (itemSlot[i] != null && itemSlot[i].item == item && itemSlot[i].quantity < item.maxStackAmount)
+                return itemSlot[i];
+        }
+
+        return null;
+    }
+
+    ItemSlot GetEmptySlot()
+    {
+        for (int i = 0; i < itemSlot.Length; i++)
+        {
+            if (itemSlot[i].item == null)
+                return itemSlot[i];
+        }
+
+        return null;
     }
 }
