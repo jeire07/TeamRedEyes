7cbf0ba3e3758088bb4c5b9c45f9e2c293605cf3
taeseongHan <taeseong0505@naver.com>
2024-01-24 18:35:26 +0900

    [test] Equipment

--- Assets/Scripts/Inventory/EquipSlot.cs
+++ Assets/Scripts/Inventory/EquipSlot.cs
@@ -5,17 +5,32 @@ using UnityEngine.UI;
 
 public class EquipSlot : MonoBehaviour
 {
+    public ItemData Item; 
     public Image clothesIcon;
     public Image weaponIcon;
     public Image shoesIcon;
 
 
-    private void Equip()
+    public void Equip(ItemData itemData)
     {
-       
+
+        Debug.Log($"{itemData.icon}");
+        if(itemData.equipableType == EquipableType.Weapon)
+        {
+            weaponIcon.sprite = itemData.icon;
+            Debug.Log($"{weaponIcon.sprite}");
+        }
+        else if (itemData.equipableType == EquipableType.Shoes)
+        {
+            shoesIcon.sprite = itemData.icon;
+        }
+        else if (itemData.equipableType == EquipableType.Clothes)
+        {
+            clothesIcon.sprite = itemData.icon;
+        }
     }
 
-    private void UnEquip()
+    public void UnEquip()
     {
        
     }
