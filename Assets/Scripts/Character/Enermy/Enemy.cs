17c058f2d20cac277a351c7f0d3fd66e8c3e095b
taeseongHan <taeseong0505@naver.com>
2024-02-22 23:05:32 +0900

    [fix] Level up function

--- Assets/Scripts/Character/Enermy/Enemy.cs
+++ Assets/Scripts/Character/Enermy/Enemy.cs
@@ -67,7 +67,6 @@ public class Enemy : MonoBehaviour
         {
             stateMachine.ChangeState(stateMachine.DeadState);
             StatManager.Instance.GainExp();
-            StatManager.Instance.LevelUp();
         }
     }
     private void HandleHealthChanged(float healthPercentage)
